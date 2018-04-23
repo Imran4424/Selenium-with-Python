866d5f5bb59c33cee7c2666e613198bf4b395f52
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-23 16:13:56 +0600

    selenium codes

---
+++ food_site_annoucement.py
@@ -0,0 +1,26 @@
+import time
+from random import randint
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Firefox()
+
+driver.get("http://localhost/foodsite/index.php")
+
+t = randint(5, 7)
+
+time.sleep(t)
+
+element_username = driver.find_element_by_css_selector(
+    "#content > form:nth-child(11) > fieldset:nth-child(1) > label:nth-child(2) > input:nth-child(3)")
+
+
+element_username.clear()
+
+element_username.send_keys("admin")
+
+element_userpass = driver.find_element_by_css_selector(
+    "#content > form:nth-child(11) > fieldset:nth-child(1) > label:nth-child(5) > input:nth-child(3)")
+
+
