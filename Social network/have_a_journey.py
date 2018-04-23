764ca1809d3ca59952ef9f86e3e010634f5383c8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-23 21:34:40 +0600

    Python selennium code

--- Social network/have_a_journey.py
+++ Social network/have_a_journey.py
@@ -8,7 +8,7 @@ driver = webdriver.Firefox()
 
 # entering the site
 
-driver.get("https://www.facebook.com/")
+driver.get("https://www.facebook.com")
 
 t = randint(8,10)
 
@@ -18,4 +18,19 @@ time.sleep(t)
 
 element_home_post = driver.find_element_by_css_selector("._1mf")
 
+element_home_post.send_keys("Hi Dipak, Are you watching, Welcome to selenium automation")
 
+time.sleep(t)
+
+# closing the posting window
+
+element_home_close = driver.find_element_by_css_selector(".sx_90723f")
+
+time.sleep(t)
+
+
+
+
+# closing the tab or browser
+
+driver.close()
