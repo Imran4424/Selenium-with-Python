a1459e9a263effc000b4bd1f607f4ac66e76841e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-23 21:27:56 +0600

    Python selennium code

--- Social network/have_a_journey.py
+++ Social network/have_a_journey.py
@@ -9,3 +9,13 @@ driver = webdriver.Firefox()
 # entering the site
 
 driver.get("https://www.facebook.com/")
+
+t = randint(8,10)
+
+time.sleep(t)
+
+# writing to homepage wall
+
+element_home_post = driver.find_element_by_css_selector("._1mf")
+
+
