6276b3ab3660e4ed6dd700411d61c2a4dc71bac3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-23 10:33:59 +0600

    Python selennium code

--- Intro.py
+++ Intro.py
@@ -1,3 +1,5 @@
+import time
+from random import randint
 from selenium import  webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -8,6 +10,10 @@ driver.get("http://www.python.org")
 
 assert "Python" in driver.title
 
+t = randint(10,15)
+
+time.sleep(t)
+
 element = driver.find_element_by_name("q")
 
 element.clear() #for clearing the search field
@@ -17,5 +23,7 @@ element.send_keys("pycon")  #giving input to search key "pycon"
 element.send_keys(Keys.RETURN)   # returning result
 
 assert "No results found." not in driver.page_source  #if there is nothing in return
+
+time.sleep(t)
 
 driver.close()
