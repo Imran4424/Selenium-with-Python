616d605d6c2ac0d7140284814284b75137164409
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-25 21:45:50 +0600

    selenium codes

--- Social network/have_a_journey_github.py
+++ Social network/have_a_journey_github.py
@@ -24,7 +24,7 @@ time.sleep(t)
 
 # scroll_down
 
-driver.execute_script("window.scrollTo(0,y)")
+driver.execute_script("window.scrollTo(0,200*1)")
 
 t = randint(7, 12)
 
@@ -33,7 +33,7 @@ time.sleep(t)
 # scroll down 2nd time in a row
 
 
-driver.execute_script("window.scrollTo(0,y)")
+driver.execute_script("window.scrollTo(0,200*2)")
 
 t = randint(7, 12)
 
