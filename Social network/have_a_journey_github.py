a2c891cf2ce4dc9d3416f359aebe94cd752e6493
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-26 01:39:15 +0600

    selenium codes

--- Social network/have_a_journey_github.py
+++ Social network/have_a_journey_github.py
@@ -10,31 +10,41 @@ driver = webdriver.Firefox()
 
 driver.get("https://github.com/")
 
-t = randint(7,12)
+#t = randint(7,12)
 
-time.sleep(t)
+#time.sleep(t)
 
 # go to profile
 
 driver.get("https://github.com/Imran4424")
 
-t = randint(7, 12)
+#t = randint(7, 12)
 
-time.sleep(t)
+#time.sleep(t)
 
 # scroll_down
 
 driver.execute_script("window.scrollTo(0,200*1)")
 
-t = randint(7, 12)
+#t = randint(7, 12)
 
-time.sleep(t)
+#time.sleep(t)
 
 # scroll down 2nd time in a row
 
 driver.execute_script("window.scrollTo(0,200*2)")
 
 t = randint(7, 12)
+
+time.sleep(t)
+
+# back to home
+
+element_home = driver.find_element_by_xpath("/html/body/div[1]/header/div/div[1]/div/a/svg")
+
+element_home.send_keys(Keys.RETURN)
+
+t = randint(7,12)
 
 time.sleep(t)
 
